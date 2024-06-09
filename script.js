1a36ab3c1cdf93cb6ac904ff3704b5b13bc61e5e
ArunNagaraj05 <arunnagaraj459@gmail.com>
2024-06-09 17:03:30 +0530

    Hell task

---
+++ script.js
@@ -0,0 +1,48 @@
+setTimeout(()=>{
+    let h3 = document.getElementById("root")
+    h3.innerHTML = '<h4>10</h4>'
+    setTimeout(()=>{
+        let h3 = document.getElementById("root")
+        h3.innerHTML = '<h4>9</h4>'
+        setTimeout(()=>{
+            let h3 = document.getElementById("root")
+            h3.innerHTML = '<h4>8</h4>'
+            setTimeout(()=>{
+                let h3 = document.getElementById("root")
+                h3.innerHTML = '<h4>7</h4>'
+                setTimeout(()=>{
+                    let h3 = document.getElementById("root")
+                    h3.innerHTML = '<h4>6</h4>'
+                    setTimeout(()=>{
+                        let h3 = document.getElementById("root")
+                        h3.innerHTML = '<h4>5</h4>'
+                        setTimeout(()=>{
+                            let h3 = document.getElementById("root")
+                            h3.innerHTML = '<h4>4</h4>'
+                            setTimeout(()=>{
+                                let h3 = document.getElementById("root")
+                                h3.innerHTML = '<h4>3</h4>'
+                                setTimeout(()=>{
+                                    let h3 = document.getElementById("root")
+                                    h3.innerHTML = '<h4>2</h4>'
+                                    setTimeout(()=>{
+                                        let h3 = document.getElementById("root")
+                                        h3.innerHTML = '<h4>1</h4>'
+                                        setTimeout(()=>{
+                                            let h3 = document.getElementById("root")
+                                            h3.innerHTML = '<h4>0</h4>'
+                                            setTimeout(()=>{
+                                                let h3 = document.getElementById("root")
+                                                h3.innerHTML = '<h2>Happy Independence Day...!</h2>'
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
